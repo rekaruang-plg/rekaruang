221530e4def90f02dacd36bcd282af9943489f86
rekaruang-plg <racingpempek@gmail.com>
2026-08-21 23:02:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Publishable/anon key boleh dipakai di browser SELAMA RLS aktif.
 // JANGAN pernah taruh service_role key di file ini.
 window.REKA_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://mbkpajhzukrbiipgeibf.supabase.co',
+  supabaseAnonKey: 'sb_publishable_e_viroHQ3qYevDwHZNEIBQ_1cGmXJuz'
 };
